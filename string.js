b39df1a63d3936592d611fc0943934559e3920ad
Cynthia-Matsitsa <cynthiamatsitsa@gmail.com>
2024-03-08 16:37:58 +0300

    add file

---
+++ string.js
@@ -0,0 +1,35 @@
+//1. Extract the last fourfrom the string below
+let string = "extravaganza";
+let substr = string.substring(8, 12);
+console.log({substr});
+
+//2.lnsert the following string at the fourth index
+const food = "The quick fox jumped over the lazy dog";
+const word = "eat";
+const index = 4;
+console.log
+
+// 3.count how many times the following string appears in the string
+const story = "The quick rown fox jumps over the lazy dog";
+console.log(story.split("the").length-1);
+console.log(story.split("brown").length-1);
+
+//4.using Javascript,find the followingwords from string
+const string1 = "The pupils are reading in the library";
+const pos1 = string1.search("are");
+console.log({pos1});
+
+const string2 = "The childwas sitting on the table before it fell";
+const pos2 = string2.search("sitting");
+console.log({pos2});
+
+//5.convert the following strings into specified format
+let text1 = "wonderful";
+let textUpperCase = text1.toUpperCase();
+console.log({textUpperCase});
+
+let text2 = ("amazing");
+let textlowercase = text2.toLowerCase();
+console.log({textlowercase});
+
+
